572c36f5585861552e9dbfb27d5d0ee0c6bde0d0
unknown <hasan.gurbuz@student.kdg.be>
2016-01-14 15:13:17 +0100

    laravel tutorial complete

--- laravel/quickstart/app/Http/routes.php
+++ laravel/quickstart/app/Http/routes.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Task;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Routes File
@@ -11,9 +14,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,48 @@ Route::get('/', function () {
 |
 */
 
-Route::group(['middleware' => ['web']], function () {
-    //
+Route::group(['middleware' => 'web'], function () {
+
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/', function () {
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+
+	    return view('tasks', [
+	        'tasks' => $tasks
+	    ]);
+    });
+
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+		]);
+
+	    if ($validator->fails()) {
+	        return redirect('/')
+	            ->withInput()
+	            ->withErrors($validator);
+	    }
+		
+		// Create The Task...
+		
+		$task = new Task;
+	    $task->name = $request->name;
+	    $task->save();
+
+	    return redirect('/');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{task}', function (Task $task) {
+        $task->delete();
+
+   		return redirect('/');
+    });
 });
